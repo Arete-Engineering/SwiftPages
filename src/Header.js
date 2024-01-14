dc25370f652dbc36a69542a847ebdb4dfaba56fd
Abdul Mendahawi <amendahawi@outlook.com>
2024-01-14 10:41:04 -0800

    google auth setup

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,32 @@
-import profile_picture from "./temp-pfp.jpg";
+import profile_pic from "./temp-pfp.jpg";
 import "./styles.css";
 
-let project_name = "Opulent";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+//Firebase config
+firebase.initializeApp({
+  apiKey: "AIzaSyCjlP_icdXnh6DbIemKIp4M7xWMkvatgKA",
+  authDomain: "omilia-f0b42.firebaseapp.com",
+  projectId: "omilia-f0b42",
+  storageBucket: "omilia-f0b42.appspot.com",
+  messagingSenderId: "11465010307",
+  appId: "1:11465010307:web:78a4221e498bfea2af7fb6",
+  measurementId: "G-8FWM5P1VDC",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+let project_name = 'Opulent';
 
 export default function Header() {
+    const user = auth.currentUser;
+    const profile_picture = user ? user.photoURL : profile_pic;
+
     return (
         <header className="App_header">
             <ul>
@@ -11,8 +34,19 @@ export default function Header() {
                 <li><a href="#">Earth</a></li>
                 <li><a href="#">Create</a></li>
                 <li><a href="#">Memory</a></li>
+                <li><SignIn /></li>
                 <li><img src={profile_picture} alt="ProfilePicture" className="profile_picture"/></li>
             </ul>
         </header>
+    )
+}
+
+function SignIn() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+    return(
+        <button onClick={signInWithGoogle}>Sign-In</button>
     )
 }
