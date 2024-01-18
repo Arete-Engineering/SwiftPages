39457f2efef8bcabf1fbb1f867e98920ca49b9d2
Abdul Mendahawi <amendahawi@outlook.com>
2024-01-18 10:36:51 -0800

    text editor styling update

--- src/components/Board.js
+++ src/components/Board.js
@@ -18,26 +18,26 @@ export default function Board() {
   };
 
   return (
-    <div style={{ backgroundColor: "#191C20" }}>
+    <div style={{ backgroundColor: "white", borderBottom: "2px solid #F1F4F7"}}>
       <ul>
         <a
-          className="btn btn-dark mr-4 mt-1 mb-1 btn-sm"
-          style={{ width: "14%", border: "1px solid #555" }}
+          className="btn btn-light mr-4 mt-1 mb-1 btn-sm"
+          style={{ width: "14%", border: "1px solid #F1F4F7", color: "#364C57" }}
           onClick={handleSaveClick}
         >
           Publish
         </a>
         <a
           href="/home"
-          className="btn btn-dark mr-4 mt-1 mb-1 btn-sm"
-          style={{ marginLeft: "20px", width: "14%", border: "1px solid #555" }}
+          className="btn btn-light mr-4 mt-1 mb-1 btn-sm"
+          style={{ marginLeft: "20px", width: "14%", border: "1px solid #F1F4F7", color: "#364C57" }}
           onClick={handleCloseClick}
         >
           Close
         </a>
         <a
-          className="btn btn-dark mt-1 mb-1 btn-sm"
-          style={{ marginLeft: "20px", width: "14%", border: "1px solid #555" }}
+          className="btn btn-light mt-1 mb-1 btn-sm"
+          style={{ marginLeft: "20px", width: "14%", border: "1px solid #F1F4F7", color: "#364C57" }}
           onClick={handleDeleteClick}
         >
           Delete
