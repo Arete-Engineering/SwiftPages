aad80d79d3bb8fa804f259bd1f40db332b62a3b0
Abdul Mendahawi <amendahawi@outlook.com>
2024-01-18 10:45:47 -0800

    text header added

---
+++ src/components/PageHeader.js
@@ -0,0 +1,9 @@
+
+
+export default function PageHeader() {
+    return (
+        <>
+        <h1 style={{color: "#223037", padding: "3%", textAlign: "left", paddingLeft:"5%", borderBottom: "4px solid green"}}>Draft</h1>
+        </>
+    )
+}
--- src/pages/TextEditor.js
+++ src/pages/TextEditor.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Editor from "../components/Editor";
 import Board from "../components/Board";
+import PageHeader from "../components/PageHeader";
 
 export default function TextEditor() {
   document.body.style = `background: white`;
   return (
     <>
+      <PageHeader />
       <Board />
       <Editor />
     </>
