7517778a69d0be8cc11ddfe157f26aa5c52d6369
Abdul Mendahawi <amendahawi@outlook.com>
2024-02-01 10:13:52 -0500

    Display Name Shown

--- src/components/UpperDashboard.js
+++ src/components/UpperDashboard.js
@@ -1,8 +1,11 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
-export default function UpperDashboard(user) {
+export default function UpperDashboard() {
+    const userName = firebase.auth().currentUser?.displayName;
     return (
         <>
-        <h1 className="" style={{padding: "5%", paddingTop: "10%", textAlign: "left"}}>Welcome back, Abdul.</h1>
+        <h4 className="" style={{padding: "5%", paddingTop: "10%", textAlign: "left"}}>Welcome back, {userName}</h4>
         </>
     )
 }
